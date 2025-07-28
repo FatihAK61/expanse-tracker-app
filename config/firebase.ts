bf7879bf90d47f80d8fc0d05ed0a10329fcbf434
Fatih AK <63843654+FatihAK61@users.noreply.github.com>
2025-07-28 19:46:19 +0300

    Update firebase.ts

--- config/firebase.ts
+++ config/firebase.ts
@@ -4,15 +4,15 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { getFirestore } from "@firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDyk3ctz3-Of7zmXpMWeX6DR5k2saH4-eU",
-  authDomain: "my-avasome-project.firebaseapp.com",
+  apiKey: "...",
+  authDomain: "...",
   databaseURL:
-    "https://my-avasome-project-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "my-avasome-project",
-  storageBucket: "my-avasome-project.firebasestorage.app",
-  messagingSenderId: "876837512297",
-  appId: "1:876837512297:web:09d7eb6a3df668d7032ef8",
-  measurementId: "G-WHGFDX8L1V",
+    "...",
+  projectId: "...",
+  storageBucket: "...",
+  messagingSenderId: "...",
+  appId: "...",
+  measurementId: "...",
 };
 
 // Initialize Firebase
